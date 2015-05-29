feaf3e1cdce2509c217e8aa37233548b8ba389ee
BoBrebel <mohamedelfadhel.elhachemi@esprit.tn>
2015-05-29 16:11:40 +0100

    form style

--- view/form.php
+++ view/form.php
@@ -14,56 +14,163 @@
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
      	<!-- Compiled and minified JavaScript -->
 		  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js"></script>
+		  <script type="text/javascript">
+		  $('.datepicker').pickadate({
+    		selectMonths: true, // Creates a dropdown to control month
+			selectYears: 15 // Creates a dropdown of 15 years to control year
+			  });
+		  </script>
 		<!-- <nav>
 		</nav> -->
 		<div class="container">
 			<div class="row">
-				<div class="col s12 m12 l12">
-					<form action="addClient.php" method="POST">
-						<label>Prénom : </label>
-						<input type="text" name="prenom" required>
-						<br>
-						<label>Nom : </label>
-						<input type="text" name="nom" required>
-						<br>
-						<label>CIN : </label>
-						<input type="text" name="cin" required>
-						<br>
-						<label>Sexe : </label>
-						<input type="radio" name="sexe" value="Homme" checked>
-							<label>Homme</label>
-						<input type="radio" name="sexe" value="Femme">
-							<label>Femme</label>
-						<br>
-						<label>Date de Naissance : </label>
-						<input type="calendar" name="date_naissance" required>
-						<br>
-						<label>Adresse : </label>
-						<!-- <input type="Adress" name> -->
-						<br>
-						<label>E-Mail : </label>
-						<input type="email" name="email">
-						<br>
-						<label>Photo : </label>
-						<input type="file" name="photo" class="btn">
-						<br>
-						<label></label>
-						<input type="checkbox" name="inclusion" value="politique">
-						<label>Inclusion Politique</label>
-						<br>
-						<input type="checkbox" name="inclusion" value="economique">
-						<label>Inclusion Economique</label>
-						<br>
-						<input type="checkbox" name="inclusion" value="sociale">
-						<label>Inclusion Sociale</label>
-						<br>
-						<input type="checkbox" name="inclusion" value="culturelle">
-						<label>Inclusion Culturelle</label>
-						<br>
-						<input type="checkbox" name="inclusion" value="technologique">
-						<label>Inclusion Technologique</label>
-						<br>
-						<input type="submit" value="Go !">
+				<div class="col s6 m6 l6 offset-s3 offset-m3 offset-l3">
+					<form action="addClient.php" method="POST" id="addClient" class="form">
+						<div class="row">
+							<div class="input-field col s12">
+						          <input id="prenom" type="text" class="validate" required>
+						          <label for="prenom">Prenom</label>
+					        </div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+						          <input id="nom" type="text" class="validate" required>
+						          <label for="nom">Nom</label>
+					        </div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+						          <input id="cin" type="text" class="validate" required>
+						          <label for="cin">CIN</label>
+					        </div>
+						</div>
+						<div class="row">
+							<div class="col s6 m6 l6">
+								<label>Sexe</label>
+							</div>
+							<div class="col s6 m6 l6">
+								<div class="row">
+									<p>
+								      <input name="sexe" type="radio" id="homme" value="homme" checked/>
+								      <label for="homme">Homme</label>
+								    </p>
+								</div>
+								<div class="row">
+									<p>
+								      <input name="sexe" type="radio" id="femme" value="femme" />
+								      <label for="femme">Femme</label>
+								    </p>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<label for="date">Date de naissance</label>
+							<input type="date" class="datepicker" id="date" required>
+						</div>
+						<div class="row">
+							<div class="input-field col s6">
+						          <input id="adresse" type="text" class="validate" required>
+						          <label for="adresse">Adresse</label>
+					        </div>
+					        <div class="input-field col s3">
+						          <input id="zipcode" type="text" class="validate" required>
+						          <label for="zipcode">Code postal</label>
+					        </div>
+					        <div class="input-field col s3">
+						          <input id="ville" type="text" class="validate" required>
+						          <label for="ville">Ville</label>
+					        </div>
+						</div>
+						<div class="row">
+							<div class="input-field col s12">
+						          <input id="email" type="email" class="validate" required>
+						          <label for="email">Email</label>
+					        </div>
+						</div>
+						<div class="row">
+							<div class="file-field input-field">
+						    	<input class="file-path validate" type="text"/>
+							      <div class="btn">
+							        <span>Photo</span>
+							        <input type="file"  required/>
+							      </div>
+						    </div>
+						</div>
+						<div class="row">
+							<div class="col s6 m6 l6">
+								<label>Statut</label>
+							</div>
+							<div class="col s6 m6 l6">
+								<div class="row">
+									<p>
+								      <input name="statut" type="radio" id="junior" value="junior"  checked/>
+								      <label for="junior">Junior(30dt)</label>
+								    </p>
+								</div>
+								<div class="row">
+									<p>
+								      <input name="statut" type="radio" id="senior" value="senior" />
+								      <label for="senior">Senior(100dt)</label>
+								    </p>
+								</div>
+								<div class="row">
+									<p>
+								      <input name="statut" type="radio" id="sponsor" value="sponsor" />
+								      <label for="sponsor">Sponsor(500Dt)</label>
+								    </p>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col s6 m6 l6">
+								<label>Inclusion</label>
+							</div>
+							<div class="col s6 m6 l6">
+								<div class="row">
+									<p>
+                                    	<input type="checkbox" id="politique" name="inclusion[]" value="politique"/>
+                                        <label for="politique">Inclusion Politique</label>
+                                    </p>
+								</div>
+								<div class="row">
+									<p>
+                                    	<input type="checkbox" id="economique" name="inclusion[]" value="economique"/>
+                                        <label for="economique">Inclusion Economique</label>
+                                    </p>
+								</div>
+								<div class="row">
+									<p>
+                                    	<input type="checkbox" id="sociale" name="inclusion[]" value="social"/>
+                                        <label for="sociale">Inclusion Sociale</label>
+                                    </p>
+								</div>
+								<div class="row">
+									<p>
+                                    	<input type="checkbox" id="culturelle" name="inclusion[]" value="culturelle"/>
+                                        <label for="culturelle">Inclusion Culturelle</label>
+                                    </p>
+								</div>
+								<div class="row">
+									<p>
+                                    	<input type="checkbox" id="technologique" name="inclusion[]" value="technologique"/>
+                                        <label for="technologique">Inclusion Technologique</label>
+                                    </p>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<p>
+	                            <input type="checkbox" id="newsletter"  checked/>
+	                            <label for="newsletter">J'accepte de recevoir d'emails de la part de Youth Decides</label>
+                            </p>
+						</div>
+						<div class="row">
+							<p>
+	                            <input type="checkbox" id="terms" required/>
+	                            <label for="terms"><a href="#">Terms&Services</a></label>
+                            </p>
+						</div>
+						<button class="btn waves-effect waves-light" type="submit" name="action" onclick="formsubmit()">Get the Card</button>
 					</form>
 				</div>
 			</div>
